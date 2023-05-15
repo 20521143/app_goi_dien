e1384d815d954860d93bf7acb078a344ccc9fab7
CongNguyen123 <nguyenthanhlong012002@gmail.com>
2023-05-16 00:08:11 +0700

    first commit

---
+++ app/src/main/java/com/camoi/goi_dien_thoai/MainActivity.java
@@ -0,0 +1,14 @@
+package com.camoi.goi_dien_thoai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+C
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
