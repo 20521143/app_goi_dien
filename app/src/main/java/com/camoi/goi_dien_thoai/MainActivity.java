7d56ef79fd0dc482bad6d138caf5c7d9b83e1cd8
SuSuneee <susufgmn27@gmail.com>
2023-05-17 22:50:56 +0700

    add call

--- app/src/main/java/com/camoi/goi_dien_thoai/MainActivity.java
+++ app/src/main/java/com/camoi/goi_dien_thoai/MainActivity.java
@@ -1,14 +1,44 @@
 package com.camoi.goi_dien_thoai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-C
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText number;
+    private ImageButton call;
+    private ImageButton contacts;
+   // static int PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        number = (EditText) findViewById(R.id.et_number);
+        call = (ImageButton) findViewById(R.id.call);
+
+       /* if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_CODE);
+        }
+*/
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phonenumber = number.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:"+phonenumber));
+                startActivity(intent);
+            }
+        });
     }
 }
